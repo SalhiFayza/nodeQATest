ee89e6953051cd72fcb94cb610a99fe45ede5261
Fayza Salhi <fayza.salhi@slashup.studio>
2025-07-29 12:35:54 +0100

    add first test

---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const myDB = require('./connection');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+
+const app = express();
+
+
+fccTesting(app); //For FCC testing purposes
+app.set('view engine','pug');
+app.set('views', __dirname + '/views/pug');
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Home',
+    message: 'Successfully rendered Pug!',
+    showLogin: false,
+    showRegistration: false,
+    showSocialAuth: false
+  });
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Listening on port ' + PORT);
+});
